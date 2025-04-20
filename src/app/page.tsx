53293816c790f4d8245418a0111985266a35ab14
Jojoussef <101554960+Jojoussef@users.noreply.github.com>
2025-04-20 04:35:45 +0100

    feat: remove user session handling from Home component for simplification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-'use client';
-
 import HomePage from './(public)/HomePage';
-import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
-    const { data: session } = useSession();
-
-    if (session) {
-        return (
-            <>
-                <p>Welcome, {session.user?.name}</p>
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
-        );
-    }
     return (
         <>
             <HomePage />
